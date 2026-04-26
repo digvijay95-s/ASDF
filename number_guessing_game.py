6f9ef895796a7a2374ffe1eaab1eb8fe5264747b
Digvijay <digvijaykumarsingh950@gmail.com>
2026-04-27 00:22:57 +0530

    professional

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,33 +1,36 @@
 import random
+def play_game():
+    low=0
+    high=100
+    num=random.randint(low,high)
+    guesses=0
+    is_rn=True
 
-low=0
-high=100
-num=random.randint(low,high)
-guesses=0
-is_rn=True
+    print("Python Number Guessing Game")
+    print(f"Select a number between {low} and {high}")
 
-print("Python Number Guessing Game")
-print(f"Select a number between {low} and {high}")
+    while is_rn:
+        guess=input("Enter your guess:")
+        
+        if guess.isdigit():
+            guess=int(guess)
+            guesses+=1
 
-while is_rn:
-    guess=input("Enter your guess:")
-    
-    if guess.isdigit():
-        guess=int(guess)
-        guesses+=1
-
-        if guess<low or guess>high:
-            print("That number is out of your Range")
-            print(f"Please select a number between {low} and {high}")
-        elif guess>num:
-            print("TOO HIGH!! try again")
-        elif guess<num:
-            print("TOO LOW!! try again")
+            if guess<low or guess>high:
+                print("That number is out of your Range")
+                print(f"Please select a number between {low} and {high}")
+            elif guess>num:
+                print("TOO HIGH!! try again")
+            elif guess<num:
+                print("TOO LOW!! try again")
+            else:
+                print("Correct Guess!!!")
+                print(f"{guess} is the right answer")
+                print(f"Guesses:{guesses}")
+                is_rn=False
         else:
-            print("Correct Guess!!!")
-            print(f"{guess} is the right answer")
-            print(f"Guesses:{guesses}")
-            is_rn=False
-    else:
-        print("INVALID INPUT!!")
-        print("Please select a number between {low} and {high}")
+            print("INVALID INPUT!!")
+            print("Please select a number between {low} and {high}")
+
+if __name__ == "__main__":
+    play_game()
